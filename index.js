d6cbef2e59ea3a84ed20b36edad6aeaccbca5951
Aaron Brooks <pleaseshutupaaron@gmail.com>
2017-06-04 04:06:06 -0400

    added search functionality, and header animation

--- index.js
+++ index.js
@@ -12,39 +12,52 @@ var RESULT_RECIPE_TEMPLATE = (
     '</figure>'
 );
 
-var RESULT_DESCRIPTION_ARRAY = [];
-
 function getDataFromApi(searchTerm, callback) {
   var query = {
-    qillIngredients: false,
+    fillIngredients: false,
     ingredients: searchTerm,
-    limitLicense: false,
+    limitLicense: true,
     number: 12,
     ranking: 1
   }
   $.getJSON(SPOONACULAR_SEARCH_URL, query, callback);
 }
 
-function getSummaryFromApi(searchTerm, callback) {
-  var query = SPOONACULAR_DESCRIPTIONS_URL.replace('{id}', searchTerm)
-  $.getJSON(query, callback);
+function getSummaryFromApi(searchTerm) {
+ 	var query = SPOONACULAR_DESCRIPTIONS_URL.replace('{id}', searchTerm)
+	return $.getJSON(query, function(data){
+		console.log(data.summary);
+		return data.summary;
+	});
 }
 
 function testCallback (data) {
 	console.log(data);
 	for (var i = 0; i < data.length; i++) {
+
 		var RECIPE_JSON_ID = data[i].id;
 		var RECIPE_JSON_IMAGE = data[i].image;
 		var RECIPE_JSON_TITLE = data[i].title;
 		var RECIPE_JSON_MISSED = data[i].missedIngredientCount;
 		var RECIPE_JSON_USED = data[i].usedIngredientCount;
-		displayRecipeTiles();
-		getSummaryFromApi(RECIPE_JSON_ID, pushSummary);
-		changeRecipeLink(RECIPE_JSON_ID, RECIPE_JSON_IMAGE);
-		changeRecipeImage(RECIPE_JSON_IMAGE);
-		changeRecipeTitle(RECIPE_JSON_TITLE);
-		changeRecipeSupplies(RECIPE_JSON_USED, RECIPE_JSON_MISSED);
+		var RESULT_RECIPE_TEMPLATE = (
+			 '<figure class="recipe-card">' +
+				'<a href='+changeRecipeLink(RECIPE_JSON_ID, RECIPE_JSON_IMAGE)+' class="recipe-link">'+
+					'<h2 class="recipe-title">'+RECIPE_JSON_TITLE+'</h2>' +
+					'<img src='+RECIPE_JSON_IMAGE+' alt="" class="recipe-image"/>' +
+					'<figcaption class="recipe-desc">' +
+						'<p class="recipe-supplies">'+changeRecipeSupplies(RECIPE_JSON_USED, RECIPE_JSON_MISSED)+'</p>' +
+						'<p class="recipe-desc">'+getSummaryFromApi(RECIPE_JSON_ID)+'</p>' +
+					'</figcaption>'+
+		        '</a>'+
+		    '</figure>'
+		   );
+		   displayRecipeTiles(RESULT_RECIPE_TEMPLATE);
 	};
+}
+
+function realCallback(data){
+	testCallback(data);
 }
 
 function changeRecipeTitle(data) {
@@ -66,28 +79,30 @@ function replaceLinkTitle(data) {
 function changeRecipeLink(data, data2) {
 	console.log(data)
 	var linkTitle = replaceLinkTitle(data2);
-	console.log(linkTitle)
-	$('a:last').attr('href', linkTitle);
+	return linkTitle
 }
 
 function changeRecipeSupplies(data1, data2) {
+	console.log(data2)
 	console.log(data1 + '/' + (data1+data2))
-	var missingIngredients = data1 + '/' + (data1+data2)
-	$('.recipe-supplies').text(missingIngredients)
+	var missingIngredients = data1 + '/' + (data1+data2);
+	return missingIngredients;
 }
 
-function displaySummary(data) {
-	$('.recipe-supplies').removeData().append('<p>' + data + '<p>');
+function displayRecipeTiles(template) {
+	$('#results').append(template);
 }
 
-function pushSummary(data) {
-	console.log (data.summary)
-	displaySummary(data.summary);
-};
-
-function displayRecipeTiles() {
-	$('#results').append(RESULT_RECIPE_TEMPLATE);
+function submitUserData() {
+	$('#ingredients-form').on('submit', function(event){
+		event.preventDefault();
+		$('#results').empty();
+		$("header").removeClass("middle");
+		var userData = $('input[type=text]').val();
+		var newUserData = userData.replace(' ', ',');
+		getDataFromApi(newUserData, realCallback);
+		$("input[type=text]").val('');
+	})
 }
 
-
-getDataFromApi('duck', testCallback);
+submitUserData();
